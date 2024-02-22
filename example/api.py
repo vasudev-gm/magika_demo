7add410586fa638e22260fe88de8071a4d2ed6a2
Vasudev Menon <vasudev1592@gmail.com>
2024-02-22 19:50:19 +0530

    - Update file type checking and detection logic.    - This MR modifies the file type checking and detection logic to improve accuracy and efficiency, with updated return statements and conditions.

--- example/api.py
+++ example/api.py
@@ -14,7 +14,8 @@ chunk_size = int(os.getenv("CHUNK_SIZE", 100))
 
 def check_file_type_magika(chunked_file: bytes) -> str:
     check_file_type = m.identify_bytes(content=chunked_file)
-    return f"File Type is {check_file_type.output.ct_label} with Mime Type {check_file_type.output.mime_type}"
+    while check_file_type is not None:
+        return f"File Type is {check_file_type.output.ct_label} with Mime Type {check_file_type.output.mime_type}"
 
 
 @api.post("/upload")
@@ -22,8 +23,8 @@ def upload(request, file: UploadedFile = File(...)) -> dict[str, Any]:
     data = file
     for chunk in data.chunks(chunk_size):
         detect_filetype = check_file_type_magika(chunk)
-    if detect_filetype is not None:
-        return {"Detected File Type": str(detect_filetype), "Size": len(data)}
+        if detect_filetype is not None:
+            return {"Detected File Type": str(detect_filetype), "Size": len(data)}
     return {"Detected File Type": "Unknown", "Size": 0}
 
 
