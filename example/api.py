6c5f5ccacf7b8249052ac5dce1db2d81bfd2da21
Vasudev Menon <vasudev1592@gmail.com>
2024-02-22 19:19:05 +0530

    Update file handling and imports for Magika functionality.  - This MR updates the file handling and imports for Magika functionality, including changing the chunk size to be read from an environment variable and updating function signatures.

--- example/api.py
+++ example/api.py
@@ -1,22 +1,24 @@
+import os
+
+from magika import Magika
 from ninja import File, NinjaAPI
 from ninja.files import UploadedFile
-from magika import Magika
 from typing_extensions import Any
 
 m = Magika()
 
 api = NinjaAPI()
 
-chunk_size = 100
+chunk_size = int(os.getenv("CHUNK_SIZE", 100))
 
 
-def check_file_type_magika(chunked_file:bytes) -> str:
+def check_file_type_magika(chunked_file: bytes) -> str:
     check_file_type = m.identify_bytes(content=chunked_file)
     return f"File Type is {check_file_type.output.ct_label} with Mime Type {check_file_type.output.mime_type}"
 
 
 @api.post("/upload")
-def upload(request, file: UploadedFile = File(...))-> dict[str, Any]:
+def upload(request, file: UploadedFile = File(...)) -> dict[str, Any]:
     data = file
     for chunk in data.chunks(chunk_size):
         detect_filetype = check_file_type_magika(chunk)
